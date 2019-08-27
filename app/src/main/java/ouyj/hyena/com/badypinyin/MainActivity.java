704f6c11a3fed6280d5d071857a785fa18f5918f
joyoverflow <joyoverflow@sina.com>
2019-08-27 22:57:19 +0800

    添加主活动和字母网格视图

--- app/src/main/java/ouyj/hyena/com/badypinyin/MainActivity.java
+++ app/src/main/java/ouyj/hyena/com/badypinyin/MainActivity.java
@@ -11,8 +11,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //获取动作条引用（隐藏动作条）
+
+        //获取动作条引用（设置动作条上的Logo图标）
         ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+        actionBar.setLogo(R.mipmap.ic_launcher);
+        //设置是否显示Logo
+        actionBar.setDisplayUseLogoEnabled(true);
+        //设置左上角图标是否显示（false则没有应用图标仅显示标题）
+        actionBar.setDisplayShowHomeEnabled(true);
+
+
     }
 }
--- app/src/main/java/ouyj/hyena/com/badypinyin/MainFragment.java
+++ app/src/main/java/ouyj/hyena/com/badypinyin/MainFragment.java
@@ -1,28 +1,56 @@
 package ouyj.hyena.com.badypinyin;
-
-
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.GridView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment
+        implements LoaderManager.LoaderCallbacks<Cursor>{
 
+    //字母网格视图
+    private GridView gridView;
+    //字母适配器对象
+    private AlphabetAdapter adapter;
 
+    /**
+     * 构造方法
+     */
     public MainFragment() {
-        // Required empty public constructor
+
+
+    }
+
+    /**
+     * 片段创建视图
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
     }
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 
+    }
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
